53c19280ed2417dd6c9efec817fa5333e8da842b
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-05-08 15:49:18 +0200

    fixed bug in jobs_run.sh

--- production/jobs_run.sh
+++ production/jobs_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/work/tools/my_env.sh
+
 # init run log
 run_log="jobs_run.log"
 rm -f $run_log
@@ -9,7 +11,7 @@ root_dir=$(pwd)
 repnames="rep1 rep2"
 for rep in $repnames; do
   cd $rep
-  jid=$(qsub -terse $pbs)
+  jid=$(qsub -terse hpipline.pbs)
   log_message "$rep: $jid" >> $run_log
   cd $root_dir
 done
