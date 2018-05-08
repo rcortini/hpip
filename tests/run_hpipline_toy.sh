ea7549fda71d38eb294a60936cd910f5a9476324
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-05-08 12:25:34 +0200

    updated toy example execution

--- tests/run_hpipline_toy.sh
+++ tests/run_hpipline_toy.sh
@@ -3,4 +3,5 @@
 #$ -M marc.corrales@crg.es
 #$ -e $PWD
 #$ -l virtual_free=32G
-python hpipline.py Toy_iPCR_rep1_fwd.fastq Toy_iPCR_rep1_Rev.fastq Toy_cDNA_rep1.fastq Toy_gDNA_rep1.fastq
+genome="/mnt/ant-login/mcorrales/HPIP/dm4R6/dmel-all-chromosome-r6.15.fasta"
+python hpipline.py Toy_iPCR_rep1_fwd.fastq Toy_iPCR_rep1_Rev.fastq prom_bcd.p $genome Toy_cDNA_rep1.fastq Toy_gDNA_rep1.fastq
