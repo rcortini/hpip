b469f8b1ab298bc108b85b32855fff7aaa9ca7fe
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-05-08 14:11:58 +0200

    new: clean_dir script in toy example

---
+++ tests/clean_dir.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -f iPCR_rep1.fasta iPCR_rep1_filtered.txt iPCR_rep1.sam iPCR_rep1_starcode.txt Toy_cDNA_rep1_spikes_starcode.txt Toy_cDNA_rep1_starcode.txt Toy_gDNA_rep1_spikes_starcode.txt Toy_gDNA_rep1_starcode.txt
