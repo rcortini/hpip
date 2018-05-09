08afe24c83d0ba4c076eb0c7257c3223de74ac63
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-05-09 11:04:36 +0200

    change permissions

--- tests/clean_dir.sh
+++ tests/clean_dir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -f iPCR_rep1.fasta iPCR_rep1_filtered.txt iPCR_rep1.sam iPCR_rep1_starcode.txt Toy_cDNA_rep1_spikes_starcode.txt Toy_cDNA_rep1_starcode.txt Toy_gDNA_rep1_spikes_starcode.txt Toy_gDNA_rep1_starcode.txt
+rm -f iPCR_rep1.fasta iPCR_rep1_filtered.txt iPCR_rep1.sam iPCR_rep1_starcode.txt Toy_cDNA_rep1_spikes_starcode.txt Toy_cDNA_rep1_starcode.txt Toy_gDNA_rep1_spikes_starcode.txt Toy_gDNA_rep1_starcode.txt hpipline_toy.err hpipline_toy.out hpiplog.txt iPCR_rep1_insertions.txt
